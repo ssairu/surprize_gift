f1e626aa39140c4c592e5d12802a71f3918df208
sairu <a.penkin2018@yandex.ru>
2023-12-26 13:44:51 +0300

    Fragments not change after screen rotation

--- app/src/main/java/com/example/surprize_gift/MainActivity.kt
+++ app/src/main/java/com/example/surprize_gift/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.surprize_gift
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
 import com.example.surprize_gift.databinding.ActivityMainBinding
 
@@ -11,32 +11,92 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var profile: AppCompatImageView
     private lateinit var homeButton : AppCompatImageView
+    private val tagsFragment = arrayListOf("HOME_FRAG", "SETTINGS_FRAG", "GIFT_FRAG", "LOGIN_FRAG", "REGISTER_FRAG")
+    private var page = "HOME_FRAG"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.main_fragment, MainFragment.newInstance())
-            .commit()
+
+        if (savedInstanceState != null && savedInstanceState.getString("PAGE")!! != "") {
+            page = savedInstanceState.getString("PAGE")!!
+        }
+        runFragByTag(page)
 
         profile = findViewById(R.id.account_header)
         profile.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.main_fragment, LoginFragment())
-                commit()
-            }
+            toLogin()
         }
 
         homeButton = findViewById(R.id.nav_home)
         homeButton.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.main_fragment, MainFragment.newInstance())
-                commit()
+            toHome()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("PAGE", whichFrag(tagsFragment))
+    }
+
+
+    private fun runFragByTag(tag: String){
+        when (tag){
+            "HOME_FRAG" -> toHome()
+            "LOGIN_FRAG" -> toLogin()
+            else -> toLogin()
+        }
+    }
+
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun toLogin(){
+        binding.navHome.setImageDrawable(resources.getDrawable(R.drawable.icon_home))
+        binding.navGift.setImageDrawable(resources.getDrawable(R.drawable.icon_present))
+        binding.navSettings.setImageDrawable(resources.getDrawable(R.drawable.icon_settings_active))
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.main_fragment, LoginFragment(), "LOGIN_FRAG")
+            commit()
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun toHome(){
+        binding.navHome.setImageDrawable(resources.getDrawable(R.drawable.icon_home_active))
+        binding.navGift.setImageDrawable(resources.getDrawable(R.drawable.icon_present))
+        binding.navSettings.setImageDrawable(resources.getDrawable(R.drawable.icon_settings))
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.main_fragment, MainFragment.newInstance(), "HOME_FRAG")
+            commit()
+        }
+    }
+
+
+
+
+    private fun whichFrag(tags: ArrayList<String>): String{
+        for (tag in tags) {
+            if (isFragActive(tag)){
+                return tag
             }
         }
 
-
+        return ""
     }
+
+    private fun isFragActive(tag: String): Boolean{
+        val myFragment = supportFragmentManager.findFragmentByTag(tag)
+        if (myFragment?.isVisible == true) {
+            return true
+        }
+        return false
+    }
+
 }
+
+
+
