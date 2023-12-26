ed92f36bf8568e0cf10d9c83818cf8647a4a1efe
Levostap <levostapenkov@gmail.com>
2023-12-26 03:21:53 +0300

    Login\Registration views

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,8 @@ android {
     namespace = "com.example.surprize_gift"
     compileSdk = 34
 
+
+
     defaultConfig {
         applicationId = "com.example.surprize_gift"
         minSdk = 24
@@ -37,10 +39,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/surprize_gift/MainActivity.kt
+++ app/src/main/java/com/example/surprize_gift/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.surprize_gift
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
