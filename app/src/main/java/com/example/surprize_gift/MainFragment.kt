203080deef814b896310c88f1ddb3e7c5a36a218
Levostap <levostapenkov@gmail.com>
2024-05-13 16:26:06 +0300

    Added getting ideas from api

--- app/src/main/java/com/example/surprize_gift/MainFragment.kt
+++ app/src/main/java/com/example/surprize_gift/MainFragment.kt
@@ -8,6 +8,19 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.surprize_gift.databinding.FragmentMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface IdeasService {
+    @GET("/api/ideas")
+    suspend fun getIdeas(): List<Idea>
+}
 
 class MainFragment : Fragment() {
 
@@ -32,13 +45,27 @@ class MainFragment : Fragment() {
         val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(view.context)
         binding.rvIdeas.layoutManager = layoutManager
 
-        val starterList: MutableList<Idea> = mutableListOf(
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.1.2:5000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(IdeasService::class.java)
+        val apiService : IdeasService = retrofit
+        runBlocking {
+            val response = withContext(Dispatchers.IO){
+                apiService.getIdeas()
+            }
+            binding.rvIdeas.adapter = IdeaAdapter(response.toMutableList())
+        }
+        /*val starterList: MutableList<Idea> = mutableListOf(
             Idea(0, "Идея 1", "Попробуйте узнать что любит ваш, близкий человек.."),
             Idea(1, "Идея 2", "Удивите себя и близких подарком с неизвестным содержимым"),
             Idea(2, "Идея 3", "Выбрать что-то для вечеринок или семейных посиделок"),
             Idea(3, "Идея 4", "Кулинарная книга с рецептами от любимого шеф-повара"),
-        )
+        )*/
 
-        binding.rvIdeas.adapter = IdeaAdapter(starterList)
+
+//        binding.rvIdeas.adapter = IdeaAdapter(starterList)
     }
 }
